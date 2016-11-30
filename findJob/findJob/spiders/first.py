9bd3a0598682dcfb5f28b975004ea25ba62ac6bb
Gavin <GavSum@Github.com>
2016-11-29 22:18:51 -0400

    Populating Scrapy Item

--- findJob/findJob/spiders/first.py
+++ findJob/findJob/spiders/first.py
@@ -5,23 +5,26 @@ from scrapy_splash import SplashRequest
 from scrapy.selector import Selector
 from scrapy.http import HtmlResponse
 from scrapy.http import Request
+from scrapy.loader import ItemLoader
+from findJob.items import jobPostItem
 
 
 class first(scrapy.Spider):
     name = "jobs"
     start_urls = ['https://www.careerbeacon.com/search/developer-jobs-in/',]
     BASE_URL = 'https://www.careerbeacon.com'
+    items = []
     
     def parse(self, response):
         counter = 0
         hitWords = ['Developer', 'Programmer',]
         for url in self.start_urls:
             yield SplashRequest(url, args={
-                'wait':5
+                'wait':2
                 },)
         articles = response.xpath('//*[@id="desktop_results"]/ul')
         for p in articles.xpath('//article'):
-            if (counter < 3):
+            if (counter < 1):
                 x = p.xpath('.//div[3]/a/div[1]/h2/text()')
                 for title in x:
                     b = re.sub(ur'[^\w]', ' ', title.extract()).strip()
@@ -33,13 +36,50 @@ class first(scrapy.Spider):
                             yield Request(url=absoluteUrl, callback=self.getData)
 
     def getData(self, response):
-        print("\n")
         self.logger.info("Visited %s", response.url)
-        test = response.xpath('//title/text()')
-        print(test)
+        item = jobPostItem()
+        infoCount = 0
+        for p in response.xpath('//*[@id="posting-body"]/article/header/div[2]/*/text()'):
+            if p:
+                infoCount = infoCount + 1
+                #Clean format
+                p = p.extract()
+                p = p.lstrip()
+                p = p.rstrip()
+                p = p.replace('\n', '')
+                p = re.sub(' +', ' ', p)
+
+                #print(p)
+
+                if(infoCount == 1):
+                    item['jobTitle'] = p
+                elif(infoCount == 2):
+                    item['sector'] = p
+                elif(infoCount == 3):
+                    item['company'] = p
+                elif(infoCount == 4):
+                    item['jobLocation'] = p
+                else:
+                    continue
+
+        print item
+        count = 0
         print("\n")
+        item['zPageData'] = {}
+        #for each entry in the page's article
+        for entry in response.xpath('//*[@id="posting-body"]/article/*'):
+            count = count +1
+            if count > 2:
+                p = entry.xpath('.//text()')
+                if p:
+                    #print(p.extract())
+                    #print("\n")
+                    item['zPageData'][count-2] = p.extract()
+
+        del item['zPageData'][count-2]
+        print item
 
 
-#Access of both title and link element from page items is working
-#Now need to either collect all the links for accessing later, or 
-#Figure out how to access each link sequentially and scrape data off of the relevant page
+
+#Acquiring data per page is working effectively, Next need to have the data sections read into a 
+#Scrapy item properly
--- findJob/ln.sh
+++ findJob/ln.sh
@@ -1,1 +1,1 @@
-scrapy shell 'http://localhost:8050/render.html?url=https://www.careerbeacon.com/posting/577866/lockheed-martin-canada/software-developer/halifax-city/&wait=5'
+scrapy shell 'http://localhost:8050/render.html?url=https://www.careerbeacon.com/posting/582752/igt/software-developer-game-developer/moncton/&wait=5'
